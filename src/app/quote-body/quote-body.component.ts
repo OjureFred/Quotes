e7234727d07636c03fec235dedf7094bf62c75d7
Fred Ojure <fredojure@hotmail.com>
2020-06-13 14:43:26 +0300

    Added quotes and code for Quote card

--- src/app/quote-body/quote-body.component.ts
+++ src/app/quote-body/quote-body.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-body',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-body.component.css']
 })
 export class QuoteBodyComponent implements OnInit {
+
+  title = 'Quotes';
+  quotes: Quote[] = [
+    new Quote(1, 'When you say YES to other make sure you are not saying NO to yourself', 'Paul Coelho',
+    'Self Awareness', 'One should not always bend over bakwards to please others. First please yourself', false, 0, 0),
+    new Quote(2, 'If you really want to catch your dream, you have to chase it', 'Unknown',
+    'Inspirational', 'You can achieve anything if you dream of it. But you have to work hard for it to become true', false, 0, 0),
+    new Quote(3, 'Life is short, break the rule, forgive quickly, kiss slowly, love truely,laugh uncontrollable and never forget anything that made you smile', 'Mark Twain',
+    'Inspirational', 'Achieve happiness by exploring and enjoying what life has to offer and not fretting over minor details', false, 0, 0),
+    new Quote(4, 'Act as if what you do makes a difference. It does', 'William James',
+    'Inspirational', 'By doing your small thing, you are adding to the big things in this world', false, 0, 0),
+  ]
 
   constructor() { }
 
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,7 @@
 export class Quote {
     showBody: boolean;
 
-    constructor(public id: number, public quote: string, public author: string, public significance: string,
+    constructor(public id: number, public quote: string, public author: string, public category: string, public significance: string,
         public like: boolean, public upvote: number, public downvote: number) {
         this.showBody = false;
     }
